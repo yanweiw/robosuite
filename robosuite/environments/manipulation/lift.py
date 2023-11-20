a492b0440bf7ab6a75fcd019fe75e85783a5e57b
Johnson Wang <johnsonwang0810@gmail.com>
2023-11-20 15:38:11 -0500

    change camera view

--- robosuite/environments/manipulation/lift.py
+++ robosuite/environments/manipulation/lift.py
@@ -407,6 +407,12 @@ class Lift(SingleArmEnv):
 
         # Arena always gets set to zero origin
         mujoco_arena.set_origin([0, 0, 0])
+        
+        mujoco_arena.set_camera(
+            camera_name="frontview",
+            pos=[0.8, 0.0, 1.3],
+            quat=[0.56, 0.43, 0.43, 0.56],
+        )
 
         # initialize objects of interest
         tex_attrib = {
